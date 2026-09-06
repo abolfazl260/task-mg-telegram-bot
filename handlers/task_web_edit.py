c6fde698aa6e18ea32e06e0e39551b06656d2ea9
abolfazlrezaie260 <abolfazl.rezaie260@gmail.com>
2026-09-06 23:41:29 +0330

    Add Telegram web edit action for tasks

---
+++ handlers/task_web_edit.py
@@ -0,0 +1,21 @@
+from urllib.parse import quote
+
+from webapp.config import WEBAPP_BASE_URL
+from webapp.report_tokens import build_report_url, create_report_token
+from services.task_service import get_task_by_id_async, user_can_modify_task_async
+from bot_context import get_current_bot_key
+
+
+async def task_web_edit_callback(update, context):
+    query = update.callback_query
+    data = query.data or ""
+    task_id = data.replace("task_web_edit_", "", 1)
+    task = await get_task_by_id_async(task_id)
+    if not task or not await user_can_modify_task_async(update.effective_user.id, task):
+        await query.answer("تسک پیدا نشد یا دسترسی ویرایش ندارید.", show_alert=True)
+        return
+
+    bot_key = get_current_bot_key() or "default"
+    token = create_report_token(bot_key, str(update.effective_user.id), report_type="task", ttl_days=30)
+    url = f"{build_report_url(WEBAPP_BASE_URL, token).rstrip('/')}/../task/{quote(token, safe='')}/{quote(str(task_id), safe='')}"
+    await query.answer(url=url)
