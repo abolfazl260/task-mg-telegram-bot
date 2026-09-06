fd45f16cbad574773b169a0e6297b0d66d171fea
abolfazlrezaie260 <abolfazl.rezaie260@gmail.com>
2026-09-06 23:41:49 +0330

    Fix Telegram web edit URL

--- handlers/task_web_edit.py
+++ handlers/task_web_edit.py
@@ -1,7 +1,7 @@
 from urllib.parse import quote
 
 from webapp.config import WEBAPP_BASE_URL
-from webapp.report_tokens import build_report_url, create_report_token
+from webapp.report_tokens import create_report_token
 from services.task_service import get_task_by_id_async, user_can_modify_task_async
 from bot_context import get_current_bot_key
 
@@ -17,5 +17,5 @@ async def task_web_edit_callback(update, context):
 
     bot_key = get_current_bot_key() or "default"
     token = create_report_token(bot_key, str(update.effective_user.id), report_type="task", ttl_days=30)
-    url = f"{build_report_url(WEBAPP_BASE_URL, token).rstrip('/')}/../task/{quote(token, safe='')}/{quote(str(task_id), safe='')}"
+    url = f"{WEBAPP_BASE_URL.rstrip('/')}/task/{quote(token, safe='')}/{quote(str(task_id), safe='')}"
     await query.answer(url=url)
